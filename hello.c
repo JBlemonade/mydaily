5eef127ae589b74f71601c19ceadda24c49d2fa7
JBlemonade <95666733+JBlemonade@users.noreply.github.com>
2022-02-07 22:15:23 +0800

    hello

---
+++ hello.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+void author_log(void);
+
+int main(void)
+{
+	int max_value;
+
+	max_value = max(555, 666);
+	printf("%d\n", max_value);
+	author_log();
+
+	getchar();
+	return 0;
+}
+
+/* 打印博主log信息 */
+void author_log(void)
+{
+	printf("author name: dingyc  \n");
+	printf("author email: dingyc_ee@yeah.net  \n");
+}
+
+
+
+
+
+
+
